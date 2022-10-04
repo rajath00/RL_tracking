88ddea20360916399d95cfefe7649c4bf3e51cf1
Rajath <rajatha@student.chalmers.se>
2022-10-04 22:06:09 +0200

    Updated code with (x,y) coordinates
    -> revised code with new method of creating environment
    --> created mask for rewards and punishments

--- src/Own_gym.py
+++ src/Own_gym.py
@@ -51,7 +51,7 @@ class Own_gym(Env):
     - -100 for going out of bounds
 
     ###Version History
-    * v0: Initial version release
+    * v2: Initial version release
     """
 
     # metadata = {
@@ -62,86 +62,53 @@ class Own_gym(Env):
     def __init__(self, render_mode: Optional[str] = None, max_step=150):
 
         self.terminated = None
-        self.num_states = 625   # total no of states
-        self.num_rows = 25
-        self.num_cols = 25
-        max_row = self.num_rows - 1
-        max_cols = self.num_cols - 1
-        #updated_target_pos = 30
+        self.num_states = 625  # total no of states
+        self.num_rows = 20
+        self.num_cols = 20
+        self.x_cor = 0
+        self.y_cor = 0
+        self.actions = {0, 1, 2, 3}
         self.num_actions = 4
-        self.P = {
-            state: {action: [] for action in range(self.num_actions)}
-            for state in range(self.num_states)
-        }
+        self.P = np.zeros((self.num_rows, self.num_cols, 2))
+        self.target_pos = np.array([int(self.num_rows / 2), int(self.num_cols / 2)])
         self.action_call()
         self.action_space = spaces.Discrete(self.num_actions)
         self.observation_space = spaces.Discrete(self.num_states)
-        self.state = None
+        self.state = (None)
         self.last_action = None
 
     # function for getting setting rewards for all the states
+
+    # def create_env(self):
+    #
+    #     x_cor = np.arange(0, self.num_rows)
+    #     y_cor = np.arange(0, self.num_cols)
+    #     self.x_cor, self.y_cor = np.meshgrid(x_cor, y_cor)
+
     def action_call(self):
-        for row in range(self.num_states):
-            for action in range(self.num_actions):
-                    reward = -1
-                    terminated = False
-                    if action == 0:
-                        if (row >= 0 and row <= 24):
-                            reward = -100
-                            new_state = row
-                        else:
-                            new_state = row - 25
-                    elif action == 1:
-                        if row >= 600 and row <= 624:
-                            reward = -100
-                            new_state = row
-                        else:
-                            new_state = row + 25
-                    elif action == 2:
-                        if row % 25 == 0:
-                            reward = -100
-                            new_state = row
-                        else:
-                            new_state = row - 1
-                    else:
-                        if row % 25 == 24:
-                            reward = -100
-                            new_state = row
-                        else:
-                            new_state = row + 1
-                    self.P[row][action].append((new_state, reward, terminated))
+        obstacles = np.ones((self.num_rows, self.num_cols), dtype=bool)
+        obstacles[1:self.num_rows - 1, 1:self.num_cols - 1] = 0
+
+        free = np.zeros((self.num_rows, self.num_cols), dtype=bool)
+        free[1:self.num_rows - 1, 1:self.num_cols - 1] = 1
+
+        self.P[:, :, 0][obstacles] = -100  # reward assignment
+        self.P[:, :, 0][free] = -1  # reward assignment
+        self.P[:, :, 1][obstacles] = 1  # termination
+        self.P[:, :, 1][free] = 0  # termination
 
     # function to set the reward func around the target
-    def target_position(self, target_pos): # XXX what is target_pos? int or tuple? it should be tuple (x,y)
-        reward = 50 # XXX why 50
-        terminated = False
+    def target_position(self, new_pos):  # XXX what is target_pos? int or tuple? it should be tuple (x,y)
+        reward = 50  # XXX why 50
+        extend_mask = 5
+        self.P[extend_mask + self.target_pos[0]:self.target_pos[0] - extend_mask, extend_mask + self.target_pos[1]:self.target_pos[1] - extend_mask, 0] = -1
+        self.P[self.target_pos[0], self.target_pos[1], 1] = 0
 
-        for j in range(1,4):
-            if j==1:
-                terminated = True
-                reward = 100
-            transitions = self.P[target_pos + (25*j)][0]
-            i = categorical_sample([t[0] for t in transitions], self.np_random)  # XXX this can be changed to Pytorch function later
-            s, r, t = transitions[i]
-            self.P[target_pos + (25*j)][0] = [(s, reward, terminated)]
+        self.P[extend_mask + new_pos[0]:new_pos[0] - extend_mask, extend_mask + new_pos[1]:new_pos[1] - extend_mask, 0] = reward
+        self.P[new_pos[0], new_pos[1], 0] = 100
+        self.P[new_pos[0], new_pos[1], 1] = 1
 
-            transitions = self.P[target_pos - (25*j)][1]
-            i = categorical_sample([t[0] for t in transitions], self.np_random)
-            s, r, t = transitions[i]
-
-            self.P[target_pos - (25*j)][1] = [(s, reward, terminated)]
-
-            transitions = self.P[target_pos + (1*j)][2]
-            i = categorical_sample([t[0] for t in transitions], self.np_random)
-            s, r, t = transitions[i]
-
-            self.P[target_pos + (1*j)][2] = [(s, reward, terminated)]
-
-            transitions = self.P[target_pos - (1*j)][3]
-            i = categorical_sample([t[0] for t in transitions], self.np_random)
-            s, r, t = transitions[i]
-
-            self.P[target_pos - (1*j)][3] = [(s, reward, terminated)]
+        self.target_pos = new_pos
 
     def encode(self):
 
@@ -156,19 +123,35 @@ class Own_gym(Env):
 
     # function to return the next state, reward and termination criteria
     def step(self, a):
-        transitions = self.P[self.state][a]
-        i = categorical_sample([t[0] for t in transitions], self.np_random)
-        s, r, t = transitions[i]
-        self.state = s
-        self.last_action = a
 
-        return int(s), r, t, False
+        next_state = (0, 0)
+        r = 0
+        t = 0
+        x = self.state[0]
+        y = self.state[1]
+        if a in self.actions:
+            if a == 0:
+                next_state = (x - 1, y)
+            elif a == 1:
+                next_state = (x + 1, y)
+            elif a == 2:
+                next_state = (x, y - 1)
+            else:
+                next_state = (x, y + 1)
+
+        self.state = next_state
+        x = self.state[0]
+        y = self.state[1]
+        self.last_action = a
+        return self.state, self.P[x, y, 0], self.P[x, y, 1], False
 
     # function to reset the position
     def reset(self, *, seed: Optional[int] = None, options: Optional[dict] = None):
 
         super().reset(seed=seed)
-        self.state = np.random.randint(0, 625)
+        init_state = np.random.randint(1,self.num_rows-2, size=(1, 2))
+        # print(init_state)
+        self.state = init_state[0]
         self.last_action = None
 
-        return int(self.state)
+        return self.state
--- src/main.py
+++ src/main.py
@@ -10,36 +10,36 @@ from matplotlib.axes import Axes
 
 
 # Function to plot the heat map for the max Q-values in each state
-def plot_values(ax: Axes, V: np.ndarray):
-    # reshape the state-value function
-    V = np.reshape(V,(25,25))
-    V = np.flip(V)
-    # plot the state-value function
-    im = ax.imshow(V, cmap='cool')
-    for (j,i),label in np.ndenumerate(V):
-        ax.text(i, j, np.round(label,1), ha='center', va='center', fontsize=5)
-    ax.tick_params(bottom='off', left='off', labelbottom='off', labelleft='off')
+# def plot_values(ax: Axes, V: np.ndarray):
+#     # reshape the state-value function
+#     V = np.reshape(V,(25,25))
+#     V = np.flip(V)
+#     # plot the state-value function
+#     im = ax.imshow(V, cmap='cool')
+#     for (j,i),label in np.ndenumerate(V):
+#         ax.text(i, j, np.round(label,1), ha='center', va='center', fontsize=5)
+#     ax.tick_params(bottom='off', left='off', labelbottom='off', labelleft='off')
 
 # create Taxi environment
 env = Own_gym()
 
-# initialize q-table
+
+# # initialize q-table
 state_size = env.observation_space.n
 action_size = env.action_space.n
-qtable = np.zeros((state_size, action_size))
-
+qtable = np.zeros((env.num_rows,env.num_cols, action_size))
 # hyper-parameters
 learning_rate = 0.9
 discount_rate = 0.8
-epsilon = 1.0
-decay_rate = 0.005
-target_move = True
+epsilon = 1
+decay_rate = 0.5
+target_move = False
 
 # training variables
 num_episodes = 1000
 max_steps = 150  # per episode
-int_pos = 199  # define initial position of the target
-
+int_pos = (5,10)# define initial position of the target
+#
 if not target_move:
     pos = int_pos
     env.target_position(pos)
@@ -47,19 +47,18 @@ if not target_move:
 
 for episode in range(num_episodes):
 
-    state = env.reset()
+    x , y = env.reset()
     done = False
-    if target_move:
-        pos = int_pos
-        env.target_position(pos)
+    # if target_move:
+    #     pos = int_pos
+    #     env.target_position(pos)
 
     for step in range(max_steps):
-
-        if target_move:
-            if step % 20 == 0:
-                pos = pos + 1
-                env.action_call()
-                env.target_position(pos)
+        # if target_move:
+        #     if step % 20 == 0:
+        #         pos = pos + 1
+        #         env.action_call()
+        #         env.target_position(pos)
 
         #exploration-exploitation tradeoff
         if random.uniform(0,1) < epsilon:
@@ -67,18 +66,22 @@ for episode in range(num_episodes):
             action = env.action_space.sample()
         else:
             # exploit
-            action = np.argmax(qtable[state, :])
-
+            action = np.argmax(qtable[x, y, :])
         # take action and observe the reward
         new_state, reward, done, truncated = env.step(action)
+        # print(new_state)
 
+        x_new = new_state[0]
+        y_new = new_state[1]
         # Q-learning algorithm
-        qtable[state, action] = qtable[state, action] + learning_rate * (
-                    reward + discount_rate * np.max(qtable[new_state, :]) - qtable[state, action])
-
+        qtable[x, y, action] = qtable[x, y, action] + learning_rate * (
+                    reward + discount_rate * np.max(qtable[x_new, y_new, :])- qtable[x, y, action])
+        #
         # Update to our new state
         state = new_state
         if done:
+            if new_state == int_pos:
+                print(state)
             break
     epsilon = np.exp(-decay_rate * episode)
 
@@ -88,25 +91,24 @@ input("Press Enter to watch trained agent...")
 # testing
 env = Own_gym()
 state = env.reset()
+print(state)
 done = False
 rewards = 0
-
 env.target_position(int_pos)
 pos = int_pos
 for step in range(max_steps):
 
-    if target_move:
-        if step % 25 == 0:
-            pos = pos + 1
-            print(f"{pos} is the new target position")
-            env.action_call()
-            env.target_position(pos)
-
+    # if target_move:
+    #     if step % 25 == 0:
+    #         pos = pos + 1
+    #         print(f"{pos} is the new target position")
+    #         env.action_call()
+    #         env.target_position(pos)
     print(f"TRAINED AGENT")
     print("Step {}".format(step + 1))
     print(f"state{state}")
-
-    action = np.argmax(qtable[state, :])
+    x , y = state
+    action = np.argmax(qtable[x, y, :])
 
     new_state, reward, done, truncated = env.step(action)
     rewards += reward
@@ -116,13 +118,15 @@ for step in range(max_steps):
     state = new_state
 
     if done:
+        print(f"state{state}")
         break
 
 env.close()
 
-#disply Final Q table
-Q = [np.max(qtable[x]) for x in range(625)]
-Q = np.reshape(Q,[625,1])
-fig1, ax1 = plt.subplots(figsize=(10,3))
-plot_values(ax1, Q)
-plt.show()
+#
+# #disply Final Q table
+# Q = [np.max(qtable[x]) for x in range(625)]
+# Q = np.reshape(Q,[625,1])
+# fig1, ax1 = plt.subplots(figsize=(10,3))
+# plot_values(ax1, Q)
+# plt.show()
